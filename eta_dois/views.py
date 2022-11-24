6776f567d8e04ad4b550e6262f83dd6fcb594d0c
franc <francelmo87@hotmail.com>
2022-11-24 09:52:03 -0300

    criada todas as views de renderização das rotas de listar,criar,atualizar e deletar da ETA Dois.

--- eta_dois/views.py
+++ eta_dois/views.py
@@ -1,3 +1,47 @@
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.urls import reverse
+
+from eta_dois.forms import EtaDoisForm
+from eta_dois.models import Etadois
+
 
 # Create your views here.
+
+def eta_dois_list(request):
+    template_name = 'eta_dois_list.html'
+    obj = Etadois.objects.all()
+    context = {'object_list': obj}
+    return render(request, template_name, context)
+
+
+def eta_dois_add(request):
+    form = EtaDoisForm(request.POST or None)
+    template_name = 'eta_dois_add.html'
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('eta_dois:eta_dois_list'))
+    context = {'form': form}
+    return render(request, template_name, context)
+
+
+def eta_dois_update(request, pk):
+    template_name = 'eta_dois_update.html'
+    obj = Etadois.objects.get(pk=pk)
+    form = EtaDoisForm(request.POST or None, instance=obj)
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('eta_dois:eta_dois_list'))
+    context = {'form': form}
+    return render(request, template_name, context)
+
+
+def eta_dois_delete(request, pk):
+    obj = Etadois.objects.get(pk=pk)
+    obj.delete()
+    return HttpResponseRedirect(reverse('eta_dois:eta_dois_list'))
+
+
+
