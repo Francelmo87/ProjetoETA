cfed4c4f13009bccd5b8c0b679bb9a75305e2d6b
franc <francelmo87@hotmail.com>
2022-11-24 10:08:05 -0300

    Incluída na rota principal do projeta a app eta três

--- projetoeta/urls.py
+++ projetoeta/urls.py
@@ -2,9 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('', include('core.urls')),
-    path('captacao/', include('captacao.urls')),
-    path('eta_um/', include('eta_um.urls')),
+    path('eta_tres/', include('eta_tres.urls')),
     path('eta_dois/', include('eta_dois.urls')),
+    path('eta_um/', include('eta_um.urls')),
+    path('captacao/', include('captacao.urls')),
+    path('', include('core.urls')),
     path('admin/', admin.site.urls),
 ]
