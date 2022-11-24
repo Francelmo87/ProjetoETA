b1ad238465072a4db6625c12e54f20727a61a3e8
franc <francelmo87@hotmail.com>
2022-11-24 10:37:29 -0300

    correção das rotas

--- eta_tres/urls.py
+++ eta_tres/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path
-from eta_dois import views as v
+from eta_tres import views as v
 
 # No template tag {% url 'app_name: url_name' objeto.pk %}
-app_name = 'eta_dois'
+app_name = 'eta_tres'
 
 
 urlpatterns = [
-    path('list/', v.eta_dois_list, name='eta_dois_list'),
-    path('add/', v.eta_dois_add, name='eta_dois_add'),
-    path('update/<int:pk>', v.eta_dois_update, name='eta_dois_update'),
-    path('delete/<int:pk>', v.eta_dois_delete, name='eta_dois_delete'),
+    path('list/', v.eta_tres_list, name='eta_tres_list'),
+    path('add/', v.eta_tres_add, name='eta_tres_add'),
+    path('update/<int:pk>', v.eta_tres_update, name='eta_tres_update'),
+    path('delete/<int:pk>', v.eta_tres_delete, name='eta_tres_delete'),
 ]
